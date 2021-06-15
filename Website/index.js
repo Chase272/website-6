b3e71a873269af33dfdfbea1ef90251798f1d27a
Chase272 <85827809+Chase272@users.noreply.github.com>
2021-06-16 00:09:40 +0530

    color change
    color change

--- Website/index.js
+++ Website/index.js
@@ -2,5 +2,9 @@ var btn = document.getElementById("Example");
 console.log("hi");
 btn.onclick = function () {
   //e.preventDefault();
-  btn.style.color = "red";
-};
+  
+   if(btn.style.color == "blue"){
+    btn.style.color = "red";
+   }
+  else {btn.style.color = "red";
+  }}
