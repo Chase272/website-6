2c2352b209e2dbb00a916ee16f32ed311230919b
Chase272 <85827809+Chase272@users.noreply.github.com>
2021-06-27 21:36:49 +0530

    Added slideshow dynamics
    added manual control only

--- Website/index.js
+++ Website/index.js
@@ -1,10 +1,29 @@
-var btn = document.getElementById("Example");
-console.log("hi");
-btn.onclick = function () {
-  //e.preventDefault();
-  
-   if(btn.style.color == "blue"){
-    btn.style.color = "red";
-   }
-  else {btn.style.color = "red";
-  }}
+console.log("hello friend");
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
